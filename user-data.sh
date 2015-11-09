aa04b069c4708977b3165ff14df9b77469ea7bb4
Matt Roberts <mattroberts297@gmail.com>
2015-11-09 17:30:41 +0000

    Update ami and user data

--- user-data.sh
+++ user-data.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-yum install -y git
-curl https://bintray.com/sbt/rpm/rpm | tee /etc/yum.repos.d/bintray-sbt-rpm.repo
-yum install -y sbt
-useradd --create-home --system akkahttp
-mkdir -p /opt/akkahttp
-chown akkahttp:akkahttp /opt/akkahttp
-su akkahttp <<EOF
+su akkahttp <<EOU
 cd /opt/akkahttp
-git clone https://github.com/mattroberts297/scalax.git .
-git checkout resilient
-sed -i '.orig' \
-'s/127\.0\.0\.1/CLOUDFORMATIONPARAM"/g'
-'src/main/resources/application.conf'
-sed -i '.orig' \
-'s/changeme/CLOUDFORMATIONPARAM"/g'
-'src/main/resources/application.conf'
-nohup sbt run &
+cat >> conf/application.conf <<EOF
+org.scalax.reactive.db {
+  dataSourceClass = "org.postgresql.ds.PGSimpleDataSource"
+  properties = {
+    url = "jdbc:postgresql://DBHOST:5432/reactive"
+    user = "reactiveapp"
+    password = "DBPASS"
+  }
+}
 EOF
-
+cd bin
+./akka-http-example &
+EOU
